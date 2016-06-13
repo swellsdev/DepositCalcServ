4e143d74cc9e479a400be6102e51252da4b02baa
Scott Wells <swellsdev@gmail.com>
2016-06-13 21:51:08 +1000

    changes

--- depositCalc/public/javascripts/charts.js
+++ depositCalc/public/javascripts/charts.js
@@ -4,16 +4,20 @@ var chart;
 var data = [4, 8, 15, 90, 16, 23, 42];
 $(document).ready(function(){
     chart = new OneBarChart(data,".chart");
+    
+    data = [16, 80, 95];
+    chart.redraw(data);
 });
 
-function OneBarChart(data,selector)
+var OneBarChart = function(data,selector)
 {   
-    function translateFunc(d, i)
+this.translateFunc = function(d, i)
     { 
-        var marginBottom = getMarginBeforeElement(data,i);
+        var marginBottom = getMarginBeforeElement(this.data,i);
+        var xScale = this.xScale;
         return "translate("+ 0 +"," + xScale(marginBottom) + ")"; 
-    }
-    
+    };
+
     function getMarginBeforeElement(data,element)
     {
         var margin = 0;
@@ -25,33 +29,60 @@ function OneBarChart(data,selector)
             }        
         }
         return margin;
-    }
+    };
     
-    var width = 60;
-    var height = 420; 
-    var data = data.sort(function(a, b){return a-b});
-    
-    var xScale = d3.scale.linear()
-        .domain([0, d3.sum(data)])
-        .range([0, height]);       
-    
-    var chart = d3.select(selector)
-        .attr("width", width)
-        .attr("height", height);
-    
-    var bar = chart.selectAll("g")
-        .data(data)
-      .enter().append("g")
-        .attr("transform", translateFunc);
-        
-    bar.append("rect")
-        .attr("width", width)
-        .attr("height", xScale);
-
-    bar.append("text")
-        .attr("x", width / 2)
-        .attr("y", $.proxy(function(d) { return xScale(d) / 2; },this))
-        .attr("dy", ".35em")
-        .text(function(d) { return d; });
+   this.width = 60;
+   this.height = 420; 
+   this.data;
+   this.xScale;
+   this.chart;
+   this.bar;
+   this.selector = selector;
+   this.redraw(data);
 }
 
+OneBarChart.prototype = function()
+{
+    
+    
+    
+    
+    var drawChart = function(newData)
+    { 
+        this.data = newData.sort(function(a, b){return a-b});
+        
+        this.xScale = d3.scale.linear()
+            .domain([0, d3.sum(this.data)])
+            .range([0, this.height]);       
+        
+        this.chart = d3.select(this.selector)
+            .attr("width", this.width)
+            .attr("height", this.height);
+        
+        this.chart.selectAll("g").remove();
+        
+        this.bar = this.chart.selectAll("g")
+            .data(this.data)
+          .enter().append("g")
+            .attr("transform", this.translateFunc);
+            
+        this.bar.append("rect")
+            .attr("width", this.width)
+            .attr("height", this.xScale);
+
+        this.bar.append("text")
+            .attr("x", this.width / 2)
+            .attr("y", $.proxy(function(d) { return this.xScale(d) / 2; },this))
+            .attr("dy", ".35em")
+            .text(function(d) { return d; });
+    };
+    
+    return{
+        redraw:drawChart
+    }
+}();
+
+
+
+
+    
